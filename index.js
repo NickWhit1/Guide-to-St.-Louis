d04d81fd21e36af960b5437c121b83e5ec900d39
NickWhit1 <nwhabibulen@yahoo.com>
2022-12-09 17:17:39 -0600

    Netlify

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function render(state = store.Home) {
 }
 function afterRender() {
   // add menu toggle to bars icon in nav bar
-  document.querySelector(".fa-bars").addEventListener("click", () => {
-    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-  });
+  // document.querySelector(".fa-bars").addEventListener("click", () => {
+  //   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  // });
 }
 router.hooks({
   before: (done, params) => {
