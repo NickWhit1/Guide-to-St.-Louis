4f949e72c87a8c0947589bc1bdb97308f74c4714
NickWhit1 <nwhabibulen@yahoo.com>
2022-12-29 19:36:46 -0600

    Middleware added

--- server/app.js
+++ server/app.js
@@ -17,11 +17,26 @@ db.once(
   console.log.bind(console, "Successfully opened connection to Mongo!")
 );
 
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
 const logging = (request, response, next) => {
   console.log(`${request.method} ${request.url} ${Date.now()}`);
   next();
 };
 
+app.use(cors);
 app.use(express.json());
 app.use(logging);
 
